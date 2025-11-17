efb926328e409ff301da5349d5bfe3d711f2793c
MystRiim <157691471+MystCely@users.noreply.github.com>
2025-11-17 14:58:02 +0100

    added DataTypesDemo1

---
+++ DatatypesDemo1/DataTypes1.cs
@@ -0,0 +1,15 @@
+namespace DatatypesDemo1;
+
+public class DataTypes1
+{
+    int number = 1; // whole numbers
+    long longNumber = 22123321312; // used for long numbers (like when you use userId)
+    float floatNumber = 1.05F; // mostly used for when you need to prioritize performance (e.g. game dev)
+    decimal decimalNumber = 1.12M; // use when you need to be very specific (e.g. money, finance, prices, etc.)
+    double doubleNumber = 1.034; // no need to add D because it's the default -> also most generally used
+
+    string username = "pandalover";
+    char letter = 'p';
+    bool isPandaCute = true;
+    
+}
---
+++ DatatypesDemo1/DataTypes2.cs
@@ -0,0 +1,12 @@
+namespace DatatypesDemo1;
+
+public class DataTypes2
+{
+    decimal calculateNum()
+    {
+    decimal a = 5;
+    decimal b = 3;
+    var sum = a + b;
+    return sum;
+    }
+}
