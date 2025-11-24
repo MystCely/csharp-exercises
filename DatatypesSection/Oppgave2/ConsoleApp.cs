f2a28905478b588aafd06ff01c6091278fb4b288
MystRiim <157691471+MystCely@users.noreply.github.com>
2025-11-24 13:30:21 +0100

    updated project names

---
+++ DatatypesSection/Oppgave2/ConsoleApp.cs
@@ -0,0 +1,12 @@
+namespace Oppgave2;
+
+public class ConsoleApp
+{
+    public decimal CalculateNum()
+    {
+        int a = 5;
+        decimal b = 3m;
+        var sum = a + b;
+        return sum;
+    }
+}
---
+++ DatatypesSection/Oppgave2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Oppgave2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var consoleApp = new ConsoleApp();
+        var result = consoleApp.CalculateNum();
+        Console.WriteLine(result);
+    }
+}
