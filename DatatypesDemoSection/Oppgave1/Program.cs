28ef59045e3a9f517d32224faece9d181d481cd7
MystRiim <157691471+MystCely@users.noreply.github.com>
2025-11-24 12:55:52 +0100

    updated file structure

---
+++ DatatypesDemoSection/Oppgave1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Oppgave1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var consoleApp = new ConsoleApp();
+        consoleApp.PrintVariables();
+        Console.WriteLine();
+        
+    }
+}
