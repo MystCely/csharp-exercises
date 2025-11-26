f6ecd34dc6d14ac89206a73036a1c6cc451569eb
MystRiim <157691471+MystCely@users.noreply.github.com>
2025-11-26 10:21:45 +0100

    updated Oppgave3 IfElseSection

--- IfElseSection/Oppgave3/ConsoleApp3.cs
+++ IfElseSection/Oppgave3/ConsoleApp3.cs
@@ -5,30 +5,23 @@ public class ConsoleApp3
     public void Run()
     {
         var input = new Input3();
-        bool methodTrue;
         int num1 = input.AskForInt("Give me a number: ");
         int num2 = input.AskForInt("Give me a second number: ");
+        int result = num1 + num2;   
         
-        int result = num1 + num2;
-
-        if (num1 == 30)
-        {
-            methodTrue = true;
-            Console.WriteLine($"The number 1 is 30, so the method returns {methodTrue}");
-        }
-        else if (num2 == 30)
-        {
-            methodTrue = true;
-            Console.WriteLine($"The number 2 is 30, so the method returns {methodTrue}");
-        } else if (result == 30)
-        {
-            methodTrue = true;
-            Console.WriteLine($"The sum of the numbers is 30, so the method returns {methodTrue}");
-        }
-        else
-        {
-            methodTrue = false;
-            Console.WriteLine($"The method returns {methodTrue} because none of the conditions are met");
-        }
+        If30(num1, num2, result);
+        
     }
+        private bool If30(int num1, int num2, int result)
+        {
+            
+            if (num1 == 30 || num2 == 30 || result == 30)
+            {
+                Console.WriteLine("The method returns true");
+                return true;
+            } 
+            
+            Console.WriteLine("The method returns false");
+            return false;
+        }
 }
